064597ea7a5197fc4d548b671c04827801b3177f
Dima Gorbachuk <dmitry.gorbachuk@gmail.com>
2020-04-24 15:18:01 +0300

    ne

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-const express = require("express")
-const app = express()
-
-app.use(express.static("public"))
+var express = require('express');
+var port = process.env.PORT || 3000;
+var app = express();
 
 app.get("/", function (req, res) {
   res.send("<h1>Hello World!</h1>")
 })
 
-app.listen(process.env.PORT || 3000, 
-	() => console.log("Server is running..."));
+app.listen(port, () => console.log("Server is running..."));
